c5dbf5c646936bd2c0cbe03cf2ca2982f245bec8
AF-Nexus <afnexus01@gmail.com>
2026-04-28 00:04:56 +0200

    Refactor imports for hooks in index.ts

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -2,5 +2,5 @@ export * from "./generated/api";
 export * from "./generated/api.schemas";
 export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
 export type { AuthTokenGetter } from "./custom-fetch";
-export { useUnseenChannelRequests } from "./hooks/useUnseenChannelRequests";
-export { useSubmitChannelRequest } from "./hooks/useSubmitChannelRequest";
+export { useUnseenChannelRequests } from "./useUnseenChannelRequests";
+export { useSubmitChannelRequest } from "./useSubmitChannelRequest";
